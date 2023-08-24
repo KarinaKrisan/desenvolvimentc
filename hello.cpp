7248f14ac5c08c6015dcf8082294398649ce4aa8
Karina Krisan <99372223+KarinaKrisan@users.noreply.github.com>
2023-08-24 13:32:25 -0300

    helloworld.cpp
    visual studio code

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+//std = standard (não é necessario utilizar sempre, somente chamar a função using namespace std no inicio do codigo)
+//cout = saida
+//cin = entrada
+//bool (boleano) = utiliza 1byte de memoria e os valores true ou false
+//char (guarda apenas 1 letra somente) = 1byte de memoria, permite armazenar um caractere ou uma cadeia de caracteres.
+//string (guardar uma frase ou uma palavra)
+//int = 4byte de memoria e armazena numeros inteiros, sem casas decimais, com valores que vão de -2.147.483.648 a 2.147.483.647
+//double = 8byte de memoria e armazena numeros com menos de 15 casas decimais, com valores que vão de 2.2e-308 a 1.79e308... numeros reais e com virgula... nao utilizar com numeros pequenos 
+//float = 4bytesde memoria e armazena numeros com menos de seis casas decimais com valores que vao de 1.2e-308 a 3.4e-38... numeros reais e com virgula
+// \n quebra de linha
+//enl = quebra de linha
+//system ("pause") = pausa até você clicar em qualquer tecla
+
+int main()
+{ cout << "Hello World!\nTudo bem?" << endl;
+
+system ("pause");
+
+return 0;}

6031e4ea820ba49901ce714a2ddc5bf979fc24bd
Karina Krisan <99372223+KarinaKrisan@users.noreply.github.com>
2023-08-24 13:33:35 -0300

    idade.cpp
    visual studio code

---
+++ idade.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int inteiro = 5;
+    cout << inteiro << endl;
+
+    float real = 5.5;
+    cout << real << endl;
+
+    double elevado = 2.5e99;
+    cout << elevado << endl; 
+
+    bool booleano = true;
+    cout << booleano << endl;
+
+    char letra = 'A';
+    cout << letra << endl;
+
+    string frase = "Sem paciencia";
+    cout << frase << endl; 
+
+    int idade; 
+    cout << "Qual sua idade?" << endl;
+    cin >> idade; 
+    string nome;
+    cout << "Qual seu nome?" << endl;
+    cin >> nome; 
+
+    return 0;
+}
